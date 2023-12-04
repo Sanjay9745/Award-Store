fb845c5afd2fd2e29ecf404dab15b7775a0e4d35
Sanjay9745 <sanjaysanthosh919@gmail.com>
2023-12-04 14:41:16 +0530

    everything fixed finalized for testing

--- frontend/src/config/SERVER_URL.js
+++ frontend/src/config/SERVER_URL.js
@@ -1,5 +1,5 @@
-const SERVER_URL = "http://localhost:3000/api"
-// const SERVER_URL = "https://award-store.onrender.com/api"
+// const SERVER_URL = "http://localhost:3000/api"
+const SERVER_URL = "https://award-store.onrender.com/api"
 
 
 export default SERVER_URL;
