8b879adee7890358963f08aa10f176be2dc98729
Sanjay9745 <sanjaysanthosh919@gmail.com>
2023-12-04 15:10:04 +0530

    orders updated

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -24,7 +24,7 @@ router.get("/all", productController.getAllProducts);
 router.get("/all-with-pagination", productController.getAllProductsWithPagination);
 router.get("/single/:id", productController.getProductById);
 router.post("/create",upload,adminAuth,productController.createProduct);//add admin auth
-router.put("/update",upload,adminAuth, productController.updateProduct);//add admin auth
+router.post("/update",upload,adminAuth, productController.updateProduct);//add admin auth
 router.delete("/delete/:id",adminAuth, productController.deleteProduct);//add admin auth
 
 module.exports = router;
